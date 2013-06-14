87d30a08a8ae92ee86ad42cac9bbb42abcf9c763
Katharina Hayer <katharinaehayer@gmail.com>
2013-06-14 14:42:54 -0400

    hitting the pause button

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import cv2,cv
 import numpy as np
+import matplotlib.pyplot as plt
 
 CV_CAP_PROP_POS_MSEC = 0
 CV_CAP_PROP_POS_FRAMES = 1
@@ -33,7 +34,13 @@ width = int(cap.get(3))
 height = int(cap.get(4))
 
 
-
+#import heatmap
+#import random
+#
+#hm = heatmap.Heatmap()
+#pts = [(random.uniform(-77.012, -77.050), random.uniform(38.888, 38.910)) for x in range(100)]
+#hm.heatmap(pts)
+#hm.saveKML("data.kml")
 
 #print frame
 #width = frame.shape[0]
@@ -51,20 +58,28 @@ perc_50_height = int((half_height-half_height*0.35)/2)
 perc_25_width = int((half_width-half_width*0.15)/2)
 perc_25_height = int((half_height-half_height*0.15)/2)
 
-first_upper_left_point1 = tuple([0,0])
-first_upper_left_point2 = tuple([half_width,half_height])
-first_upper_left_75_point1 = tuple([perc_75_width,perc_75_height])
-first_upper_left_75_point2 = tuple([half_width-perc_75_width,half_height-perc_75_height])
-first_upper_left_50_point1 = tuple([perc_50_width,perc_50_height])
-first_upper_left_50_point2 = tuple([half_width-perc_50_width,half_height-perc_50_height])
-first_upper_left_25_point1 = tuple([perc_25_width,perc_25_height])
-first_upper_left_25_point2 = tuple([half_width-perc_25_width,half_height-perc_25_height])
+upper_left_point1 = tuple([0,0])
+upper_left_point2 = tuple([half_width,half_height])
+upper_left_75_point1 = tuple([perc_75_width+15,perc_75_height+5])
+upper_left_75_point2 = tuple([half_width-perc_75_width+15,half_height-perc_75_height-5])
+upper_left_50_point1 = tuple([perc_50_width+15,perc_50_height+5])
+upper_left_50_point2 = tuple([half_width-perc_50_width+15,half_height-perc_50_height-5])
+upper_left_25_point1 = tuple([perc_25_width+15,perc_25_height+5])
+upper_left_25_point2 = tuple([half_width-perc_25_width+15,half_height-perc_25_height-5])
+upper_right_point1 = tuple([half_width,0])
+upper_right_point2 = tuple([width,half_height])
+upper_right_75_point1 = tuple([half_width+perc_75_width-15,perc_75_height+5])
+upper_right_75_point2 = tuple([width-perc_75_width-15,half_height-perc_75_height-5])
+upper_right_50_point1 = tuple([half_width+perc_50_width-15,perc_50_height+5])
+upper_right_50_point2 = tuple([width-perc_50_width-15,half_height-perc_50_height-5])
+upper_right_25_point1 = tuple([half_width+perc_25_width-15,perc_25_height+5])
+upper_right_25_point2 = tuple([width-perc_25_width-15,half_height-perc_25_height-5])
 
-first_upper_right_point1 = tuple([half_width,0])
-first_upper_right_point2 = tuple([width,half_height])
 
 _,frame2 = cap.read()
 
+x = []
+y = []
 
 # convert to hsv and find range of colors
 #hsv = cv2.cvtColor(frame_cross,cv2.COLOR_BGR2HSV)
@@ -73,7 +88,7 @@ _,frame2 = cap.read()
 print half_width
 print half_height
 frame_number = cap.get(CV_CAP_PROP_POS_FRAMES)
-while(frame_number < 1550):
+while(frame_number < 5000):
     frame_number = cap.get(CV_CAP_PROP_POS_FRAMES)
     # read the frames
     _,frame = cap.read()
@@ -125,10 +140,12 @@ while(frame_number < 1550):
     end = tuple([half_width,height])
     cv2.line(frame,start,end,cv.CV_RGB(255,0,255))
     cv2.line(frame,start,end,cv.CV_RGB(255,0,255))
-    cv2.rectangle(frame,first_upper_left_75_point1,first_upper_left_75_point2,cv.CV_RGB(255,0,255))
-    cv2.rectangle(frame,first_upper_left_50_point1,first_upper_left_50_point2,cv.CV_RGB(255,0,255))
-    cv2.rectangle(frame,first_upper_left_25_point1,first_upper_left_25_point2,cv.CV_RGB(255,0,255))
-    cv2.line(frame,first_upper_right_point1,first_upper_right_point2,cv.CV_RGB(255,0,255))
+    cv2.rectangle(frame,upper_left_75_point1,upper_left_75_point2,cv.CV_RGB(255,0,255))
+    cv2.rectangle(frame,upper_left_50_point1,upper_left_50_point2,cv.CV_RGB(255,0,255))
+    cv2.rectangle(frame,upper_left_25_point1,upper_left_25_point2,cv.CV_RGB(255,0,255))
+    cv2.rectangle(frame,upper_right_75_point1,upper_right_75_point2,cv.CV_RGB(255,0,0))
+    cv2.rectangle(frame,upper_right_50_point1,upper_right_50_point2,cv.CV_RGB(255,0,0))
+    cv2.rectangle(frame,upper_right_25_point1,upper_right_25_point2,cv.CV_RGB(255,0,0))
 
     for cnt in contours2:
         summe = [0,0]
@@ -139,9 +156,12 @@ while(frame_number < 1550):
             number_of_points = number_of_points + 1
             summe = [summe[0]+point[0][0],summe[1]+point[0][1]]
         mid_point = [summe[0]/number_of_points,summe[1]/number_of_points]
+        x.append(mid_point[0])
+        y.append(mid_point[1])
         mid_point = tuple([int(mid_point[0]),int(mid_point[1])])
         cv2.circle(frame2,mid_point, 1, cv.CV_RGB(255,0,0))
         cv2.circle(frame,mid_point, 1, cv.CV_RGB(255,0,0))
+
     #print contours
     # finding centroids of best_cnt and draw a circle there
     #M = cv2.moments(best_cnt)
@@ -149,12 +169,30 @@ while(frame_number < 1550):
     #cv2.circle(frame,(cx,cy),5,255,-1)
 
     # Show it, if key pressed is 'Esc', exit the loop
-    cv2.imshow('frame',frame)
+
     cv2.imshow('thresh',thresh2)
     cv2.imshow('contour',frame2)
+    cv2.imshow('frame',frame)
     if cv2.waitKey(33)== 27:
         break
 cv2.imwrite("tra.png",frame2)
 # Clean up everything before leaving
 cv2.destroyAllWindows()
 cap.release()
+
+
+#
+## Generate some test data
+#
+#
+heatmap, xedges, yedges = np.histogram2d(x, y, bins=50)
+extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+#
+plt.clf()
+
+plt.imshow(heatmap, extent=extent)
+cb = plt.colorbar()
+cb.set_label('mean value')
+plt.savefig("heatmap.png")
+
+
